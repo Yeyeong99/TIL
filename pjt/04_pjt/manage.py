7c50f57c0a7ba9a41f127454feee326dbb4b3881
Yeyeong99 <67695343+Yeyeong99@users.noreply.github.com>
2025-06-08 13:07:25 +0900

    [Upload] Change Directory Name

--- pjt/04_pjt/manage.py
+++ pjt/04_pjt/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'public_opinion.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scrapy.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
